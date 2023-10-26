f8debfd8c11033f0908975294f3425874b38d684
Schaffner <emilien.schaffner@six-group.com>
2023-10-26 20:06:26 +0200

    Removed tests from other exercises

--- src/test/java/TennisTest.java
+++ src/test/java/TennisTest.java
@@ -68,39 +68,4 @@ public class TennisTest {
         checkAllScores(player1Points, player2Points, expectedScore, game);
     }
 
-    @ParameterizedTest
-    @MethodSource("getAllScores")
-    public void checkAllScoresTennisGame2(int player1Points, int player2Points, String expectedScore) {
-        TennisGame2 game = new TennisGame2("player1", "player2");
-        checkAllScores(player1Points, player2Points, expectedScore, game);
-    }
-
-    @ParameterizedTest
-    @MethodSource("getAllScores")
-    public void checkAllScoresTennisGame3(int player1Points, int player2Points, String expectedScore) {
-        TennisGame3 game = new TennisGame3("player1", "player2");
-        checkAllScores(player1Points, player2Points, expectedScore, game);
-    }
-
-    @ParameterizedTest
-    @MethodSource("getAllScores")
-    public void checkAllScoresTennisGame4(int player1Points, int player2Points, String expectedScore) {
-        TennisGame game = new TennisGame4("player1", "player2");
-        checkAllScores(player1Points, player2Points, expectedScore, game);
-    }
-
-    @ParameterizedTest
-    @MethodSource("getAllScores")
-    public void checkAllScoresTennisGame5(int player1Points, int player2Points, String expectedScore) {
-        TennisGame game = new TennisGame5("player1", "player2");
-        checkAllScores(player1Points, player2Points, expectedScore, game);
-    }
-
-
-    @ParameterizedTest
-    @MethodSource("getAllScores")
-    public void checkAllScoresTennisGame6(int player1Points, int player2Points, String expectedScore) {
-        TennisGame game = new TennisGame6("player1", "player2");
-        checkAllScores(player1Points, player2Points, expectedScore, game);
-    }
 }
